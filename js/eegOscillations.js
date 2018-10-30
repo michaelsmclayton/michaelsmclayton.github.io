f60fb645c7721a5994c8744fec7b57b69f81fe38
Michael Clayton <michaelclayton@Michaels-MacBook-Pro.local>
2018-10-30 12:27:53 +0000

    Update

--- js/eegOscillations.js
+++ js/eegOscillations.js
@@ -95,7 +95,7 @@ function SineWaveGenerator(options) {
     var amp = this.amplitude;
    
     // Center the waves
-    var yAxis = this.height * .5; 
+    var yAxis = this.height * .75; 
     
     // Styles
     this.ctx.lineWidth = lineWidth * this.dpr;
