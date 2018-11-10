6737eb39261c48baadb671ca91df1fb4690b7d76
Michael Clayton <michaelclayton@Michaels-MacBook-Pro.local>
2018-11-10 08:52:43 +0000

    Update

--- nodescripts/citationCounts.js
+++ nodescripts/citationCounts.js
@@ -1,1 +1,1 @@
-var ticsCitations = 105; var firstFrontiersCitations = 28; var ejnCitations = 8; var secondFrontiersCitations = 1; var jepgCitations = ' ';
+var ticsCitations = 106; var firstFrontiersCitations = 28; var ejnCitations = 8; var secondFrontiersCitations = 2; var jepgCitations = ' ';
