0be45558d6babd67c4c9e064cfecb760170a7130
Michael Clayton <michaelclayton@Michaels-MacBook-Pro.local>
2018-11-23 10:45:37 +0000

    JEP:G paper information edited

--- updateCitations.sh
+++ updateCitations.sh
@@ -2,4 +2,6 @@ cd ~/Documents/WebsiteDevelopment/michaelsmclayton.github.io/
 cd nodescripts
 /usr/local/bin/node getCitationCounts.js
 cd ..
-sh gitpush.sh
+git add ./nodescripts/citationCounts.js
+git commit -m "UpdateCitationCounts"
+git push
