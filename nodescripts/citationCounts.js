af8a8283889dd74212c64809594554fc23c22113
Michael Clayton <michael.clayton@unit9.com>
2019-03-19 14:00:02 +0000

    UpdateCitationCounts

--- nodescripts/citationCounts.js
+++ nodescripts/citationCounts.js
@@ -1,1 +1,1 @@
-var ticsCitations = 127; var firstFrontiersCitations = 30; var ejnCitations = 10; var secondFrontiersCitations = 2; var jepgCitations = ' ';
+var ticsCitations = 129; var firstFrontiersCitations = 30; var ejnCitations = 10; var secondFrontiersCitations = 2; var jepgCitations = ' ';
