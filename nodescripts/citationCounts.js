146dc4f4ffcdd139d7faacb5ba0b4baac9aec853
Michael Clayton <michael.clayton@unit9.com>
2020-11-10 14:00:02 +0000

    UpdateCitationCounts

--- nodescripts/citationCounts.js
+++ nodescripts/citationCounts.js
@@ -1,1 +1,1 @@
-var ticsCitations = 237; var firstFrontiersCitations = 47; var ejnCitations = 47; var jepgCitations = 8;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,33,47,76,89,90]
+var ticsCitations = 238; var firstFrontiersCitations = 47; var ejnCitations = 47; var jepgCitations = 8;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,33,47,76,90,90]
