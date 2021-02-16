7df73bf4509f48d8cf685161a9600bd9e6e68600
giuliamaistrello <michael.clayton@unit9.com>
2021-02-16 14:00:02 +0000

    UpdateCitationCounts

--- nodescripts/citationCounts.js
+++ nodescripts/citationCounts.js
@@ -1,1 +1,1 @@
-var ticsCitations = 258; var firstFrontiersCitations = 47; var ejnCitations = 54; var jepgCitations = 12;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,32,47,76,94,105,13]
+var ticsCitations = 259; var firstFrontiersCitations = 47; var ejnCitations = 55; var jepgCitations = 12;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,32,47,76,94,105,15]
