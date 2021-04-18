a54b60ddd31a8cbf97b8f6c0273c1107a7ab3f32
giuliamaistrello <maisjulia@yahoo.co.uk>
2021-04-18 14:00:02 +0100

    UpdateCitationCounts

--- nodescripts/citationCounts.js
+++ nodescripts/citationCounts.js
@@ -1,1 +1,1 @@
-var ticsCitations = 271; var firstFrontiersCitations = 51; var ejnCitations = 64; var jepgCitations = 12;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,32,47,76,97,105,37]
+var ticsCitations = 276; var firstFrontiersCitations = 51; var ejnCitations = 65; var jepgCitations = 12;var secondFrontiersCitations = 8; var citationsAcrossYears = [10,32,47,76,96,106,43]
